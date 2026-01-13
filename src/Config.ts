75a4eba9071222e6a2e55d1fcf762a5fabbaa1a3
Sivakumar Manimaran <sivakumar.manimaran@syncfusion.com>
2026-01-13 14:40:25 +0530

    Remove unwanted Changes

--- src/Config.ts
+++ src/Config.ts
@@ -2,13 +2,13 @@
 // Licensed under the MIT License.
 
 const config = {
-  appId: 'YOUR_APP_ID',
-  redirectUri: 'http://localhost:3000',
-  scopes: [
-    'user.read',
-    'mailboxsettings.read',
-    'calendars.readwrite'
-  ]
-};
+    appId: 'YOUR_APP_ID',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
 
-export default config;
+  export default config;
