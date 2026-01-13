426562d16c04890fd8f8af072f4f04df73141696
Sivakumar Manimaran <sivakumar.manimaran@syncfusion.com>
2026-01-13 12:47:20 +0530

    Remove the extra spaces

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,8 +6,7 @@ import { PublicClientApplication } from '@azure/msal-browser';
 let pca: PublicClientApplication;
 
 test('renders learn react link', () => {
-
-  render(<App pca={pca} />);
+  render(<App pca={pca}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
